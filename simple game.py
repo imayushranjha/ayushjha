831ee94f58c720ad9c15c8d6c8c8eb32ade5f9fa
imayushranjha <2223156@kmc.du.ac.in>
2024-07-04 12:42:17 +0530

    Add files via upload

---
+++ simple game.py
@@ -0,0 +1,57 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+choice1 = input("You're at a crossroad. Where you want to go?\n    Type \"Left\" or \"Right\" \n").lower()
+if choice1 == "left" :
+          choice2 = input("You have came to a lake. There is an island in the middle of the island\n  Type \"Wait\" to wait for the boat or Type \"Swim\" to swim to the island\n").lower()
+          if choice2 == "wait" :
+                    choice3 = input("You arrived at the island unharmed.There is a house with three doors one red one yellow and one blue.Which colour you choose (Red,Blue or Red)\n").lower()
+                    if choice3 == "yellow":
+                              print("You Win")
+                    elif choice3 == "red":
+                              print("Burned by fire,Game Over.")
+                    elif choice3 == "blue":
+                              print("Eaten by beasts.Game Over.")
+                    else:
+                              print("Game over")
+          else:
+                    print("Attacked by trout.Game Over.")
+else:
+          print("Fall into a hole,Game Over.")
+          
+
+                                   
+          
+
+          
+
+          
+
+
+
+
